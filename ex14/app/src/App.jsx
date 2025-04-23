ed73f59550e807f9795134640b0f0a5ade354ba8
5tarun3 <tarun.devrath@gmail.com>
2025-04-23 18:23:22 +0530

    final commit.

--- ex14/app/src/App.jsx
+++ ex14/app/src/App.jsx
@@ -3,15 +3,15 @@ import ImageDisplay from './assets/ImageDisplay';
 import Form from './assets/Form';
 import ProfilePage from './assets/ProfilePage';
 function App() {
-return (
-<div>
-<ImageDisplay />
-<hr />
-<Form />
-<hr />
-<ProfilePage />
-<hr />
-</div>
-);
+    return (
+        <div>
+            <ImageDisplay />
+            <hr />
+            <Form />
+            <hr />
+            <ProfilePage />
+            <hr />
+        </div>
+    );
 }
 export default App;
